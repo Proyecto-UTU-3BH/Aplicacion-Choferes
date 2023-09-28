9dda5595d736472144eb0e0b633f858cf8d96baa
KRaf22 <rafagonzalezknappe@gmail.com>
2023-09-28 15:13:21 -0300

    Conexion con Api OAuth para login, permitir paso si las credenciales son validas, devolver error si no lo son, guardado del token en local storage si el inicio de sesion es valido

---
+++ JavaScript/login.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginForm = document.getElementById("loginForm");
+
+    let token = localStorage.getItem('access_token');
+    if (token!=null) {
+        location.href="homepageCamionero.html"
+    }
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault(); 
+
+        let email = document.getElementById("usuario").value;
+        let password = document.getElementById("password").value;
+
+        let info = {
+            "username": email,
+            "password": password,
+            "grant_type": "password",
+            "client_id": 1,
+            "client_secret": "x21mzlq0ijQMy6IewvJcp5X9pzxjo79rfrldaboD"
+        }
+
+        fetch("http://localhost:8000/oauth/token", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams(info).toString(),
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json(); 
+            } else {
+                Swal.fire({
+                    title: 'Credenciales inválidas',
+                    text: 'Por favor, verifique su correo electrónico y contraseña.',
+                    icon: 'error',
+                    confirmButtonText: 'Ok'
+                });
+            }
+        })
+        .then((data) => {
+            localStorage.setItem("access_token", data.access_token);
+            location.href = "homepageCamionero.html";
+        })
+        .catch((error) => {
+            console.error("Error de red: ", error);
+        });
+    }); 
+});
