f787679e0c8e6f6976e16531fffba77d8c171f5f
KRaf22 <rafagonzalezknappe@gmail.com>
2023-09-28 15:30:21 -0300

    Conexion con Api Oauth para cerrar sesión, tambien manejar casos de inicio de sesion con usuario de tipo distinto a chofer e intentar ingresar a alguna url sin haber iniciado sesion previamente

---
+++ JavaScript/cerrarSesion.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const token = localStorage.getItem("access_token");
+    if (token == null) {
+        location.href = "index.html";
+    } else {
+        const headers = {
+            "Authorization": "Bearer " + token
+        };
+
+        fetch("http://localhost:8000/api/validate", {
+            method: "GET",
+            headers: headers
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json(); 
+                } 
+            })
+            .then((userData) => {
+                if (userData.tipo != "chofer") {
+                    localStorage.removeItem("access_token");
+                    location.href = "index.html";
+                } 
+            })
+            .catch((error) => {
+                console.error("Error de red: ", error);
+            });
+    }
+
+    const cerrarSesion = document.getElementById("logOut");
+
+    cerrarSesion.addEventListener("click", () => {
+        const headers = {
+            "Authorization": "Bearer " + token 
+        };
+
+        fetch("http://localhost:8000/api/logout", {
+            method: "GET",
+            headers: headers
+        })
+            .then((response) => {
+                if (response.ok) {
+                    localStorage.removeItem("access_token");
+                    location.href = "index.html";
+                } else {
+                    console.error("Error en la solicitud de cierre de sesión.");
+                }
+            })
+            .catch((error) => {
+                console.error("Error de red: ", error);
+            });
+    });
+});
