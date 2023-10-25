55df35e94e5afc57845857cbe2e4e334067954ca
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-25 17:42:47 -0300

    Se realizan modificaciones en estructura de js de lotes (no hay mas api fetch a validacion, ya que se hace en vehiculos.js)

--- JavaScript/lotes.js
+++ JavaScript/lotes.js
@@ -19,14 +19,18 @@ function mostrarLotes(data) {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    let idUsuario;
+
+    const userData = JSON.parse(sessionStorage.getItem("userData"));
+    let idUsuario = userData.idUsuario;
+
     const token = localStorage.getItem("access_token");
-    const urlValidar = "http://localhost:8000/api/validate";
     const headers = {
         "Authorization": "Bearer " + token,
     };
 
-    fetch(urlValidar, {
+    const urlLotes = "http://localhost:8001/api/usuarios/verLotes/" + idUsuario;
+
+    fetch(urlLotes, {
         method: "GET",
         headers: headers,
     })
@@ -36,34 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             throw new Error("Error en la solicitud");
         }
-    })
-    .then(data => {
-        const userData = {
-            primer_nombre: data.primer_nombre,
-            primer_apellido: data.primer_apellido,
-            segundo_apellido: data.segundo_apellido,
-            usuario: data.usuario,
-            idUsuario: data.id,
-            tipo: data.tipo,
-            calle: data.calle,
-            ci: data.ci,
-            numero_de_puerta: data.numero_de_puerta
-        };
-
-        sessionStorage.setItem("userData", JSON.stringify(userData));
-
-        idUsuario = data.id;
-
-        const urlLotes = "http://localhost:8001/api/usuarios/verLotes/"+idUsuario;
-        return fetch(urlLotes, {
-            method: "GET",
-            headers: headers,
-        });
-    })
-    .then(async response => {
-        if (response.ok) {
-            return response.json();
-        } 
     })
     .then(data => {
         if (data.length === 0) {
@@ -79,5 +55,4 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch(error => {
         console.error("Error:", error);
     });
-    
 });
