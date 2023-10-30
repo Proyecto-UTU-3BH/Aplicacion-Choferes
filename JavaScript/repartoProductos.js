7191eabd4aa1ce225a9f20054ec2db0f7570bd36
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-30 20:24:39 -0300

    Alerta por si no hay productos para repartir en la fecha que se hace la consulta

--- JavaScript/repartoProductos.js
+++ JavaScript/repartoProductos.js
@@ -1,11 +1,11 @@
-function mostrarPaquetes (data) {
+function mostrarPaquetes(data) {
 
-    const tbody = document.getElementById("productos");
+  const tbody = document.getElementById("productos");
 
-    let htmlToAppend = '';
+  let htmlToAppend = '';
 
-    data.forEach(producto => {
-        htmlToAppend += `
+  data.forEach(producto => {
+    htmlToAppend += `
             <tr>
                 <td>${producto.id}</td>
                 <td>${producto.destino}</td>
@@ -19,40 +19,48 @@ function mostrarPaquetes (data) {
                 <td>${producto.estado}</td>
             </tr>
         `;
-    });
+  });
 
-    tbody.innerHTML = htmlToAppend;
+  tbody.innerHTML = htmlToAppend;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    const userData = JSON.parse(sessionStorage.getItem("userData"));
-    let idUsuario = userData.idUsuario;
+  const userData = JSON.parse(sessionStorage.getItem("userData"));
+  let idUsuario = userData.idUsuario;
 
-    const token = localStorage.getItem("access_token");
-    const urlRepartoPaquetes = "http://localhost:8001/api/usuarios/"+idUsuario+"/verProductos";
-    const headers = {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/json"
-    };
+  const token = localStorage.getItem("access_token");
+  const urlRepartoPaquetes = "http://localhost:8001/api/usuarios/" + idUsuario + "/verProductos";
+  const headers = {
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json"
+  };
 
-    fetch(urlRepartoPaquetes, {
-        method: "GET",
-        headers: headers,
-      })
-        .then(async (response) => {
-          if (response.ok) {
-            return response.json(); 
-          } else {
-            const errorMessage = await response.text();
-            console.error("Error en la solicitud:", errorMessage);
-            throw new Error("Error en la solicitud");
-          }
-        })
-        .then((data) => {
-          mostrarPaquetes(data);
-        })
-        .catch((error) => {
-          console.error("Error:", error);
+  fetch(urlRepartoPaquetes, {
+    method: "GET",
+    headers: headers,
+  })
+    .then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        const errorMessage = await response.text();
+        console.error("Error en la solicitud:", errorMessage);
+        throw new Error("Error en la solicitud");
+      }
+    })
+    .then((data) => {
+      if (data.length === 0) {
+        Swal.fire({
+          icon: 'info',
+          title: 'No hay productos para repartir',
+          text: 'No se encontraron productos para repartir en esta fecha.',
         });
+      } else {
+        mostrarPaquetes(data);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
