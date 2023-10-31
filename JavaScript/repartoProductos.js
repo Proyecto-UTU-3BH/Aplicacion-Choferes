54f4b2998994cde80baffc666bc185b62a1c09b7
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-30 22:30:40 -0300

    Casillas para marcar los paquetes como Entregados, mediante confirmacion con sweet alert y api fetch con api transito

--- JavaScript/repartoProductos.js
+++ JavaScript/repartoProductos.js
@@ -5,6 +5,10 @@ function mostrarPaquetes(data) {
   let htmlToAppend = '';
 
   data.forEach(producto => {
+    const estaEnDomicilio = producto.estado == "en domicilio";
+    console.log("Estado es " + producto.estado)
+    console.log(estaEnDomicilio);
+
     htmlToAppend += `
             <tr>
                 <td>${producto.id}</td>
@@ -17,12 +21,69 @@ function mostrarPaquetes(data) {
                 <td>${producto.peso}</td>
                 <td>${producto.forma_entrega}</td>
                 <td>${producto.estado}</td>
+                <td>
+                  <input type="checkbox" class="marcarEntregado" value="${producto.id}" ${estaEnDomicilio ? 'checked' : ''} />
+                </td>
             </tr>
         `;
   });
 
   tbody.innerHTML = htmlToAppend;
+
+  preguntarConfirmacion();
+
 }
+
+function preguntarConfirmacion() {
+
+  document.querySelectorAll(".marcarEntregado").forEach(function (casilla) {
+    casilla.addEventListener("click", function () {
+      const idProducto = this.value;
+      if (this.checked) {
+        Swal.fire({
+          title: "Confirmación de Paquete Entregado",
+          text: "¿Este paquete ha sido entregado?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#4CAF50",
+          cancelButtonColor: "#3085d6",
+          confirmButtonText: "Sí, paquete en domicilio",
+          cancelButtonText: "Cancelar"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            marcarComoEntregado(idProducto);
+          } else {
+            this.checked = false;
+          }
+        });
+      }
+    });
+  });
+}
+
+function marcarComoEntregado(idProducto) {
+  const token = localStorage.getItem("access_token");
+  const urlMarcarComoEntregado = "http://localhost:8001/api/productos/modificar/" + idProducto;
+  const headers = {
+    "Authorization": "Bearer " + token,
+  };
+
+  fetch(urlMarcarComoEntregado, {
+    method: "GET",
+    headers: headers,
+  })
+    .then(async response => {
+      if (response.ok) {
+        location.reload();
+      } else {
+        const errorMessage = await response.text();
+        console.error("Error en la solicitud:", errorMessage);
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+};
 
 document.addEventListener("DOMContentLoaded", function () {
 
