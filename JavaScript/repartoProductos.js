1f8641fa03883af11ced9069fc7b0950d912593c
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-31 16:54:43 -0300

    Se hace fetch para modificar fecha de realizacion de un reparto cuando el usuario confirme que el paquete esta entregado

--- JavaScript/repartoProductos.js
+++ JavaScript/repartoProductos.js
@@ -5,9 +5,7 @@ function mostrarPaquetes(data) {
   let htmlToAppend = '';
 
   data.forEach(producto => {
-    const estaEnDomicilio = producto.estado == "en domicilio";
-    console.log("Estado es " + producto.estado)
-    console.log(estaEnDomicilio);
+    let estaEnDomicilio = producto.estado == "en domicilio";
 
     htmlToAppend += `
             <tr>
@@ -22,7 +20,7 @@ function mostrarPaquetes(data) {
                 <td>${producto.forma_entrega}</td>
                 <td>${producto.estado}</td>
                 <td>
-                  <input type="checkbox" class="marcarEntregado" value="${producto.id}" ${estaEnDomicilio ? 'checked' : ''} />
+                  <input type="checkbox" class="marcarEntregado" value="${producto.id}" data-reparte-id="${producto.reparte.id}" ${estaEnDomicilio ? 'checked' : ''} />
                 </td>
             </tr>
         `;
@@ -38,7 +36,9 @@ function preguntarConfirmacion() {
 
   document.querySelectorAll(".marcarEntregado").forEach(function (casilla) {
     casilla.addEventListener("click", function () {
-      const idProducto = this.value;
+      let idProducto = this.value;
+      let idReparte = this.dataset.reparteId;
+
       if (this.checked) {
         Swal.fire({
           title: "Confirmación de Paquete Entregado",
@@ -51,7 +51,7 @@ function preguntarConfirmacion() {
           cancelButtonText: "Cancelar"
         }).then((result) => {
           if (result.isConfirmed) {
-            marcarComoEntregado(idProducto);
+            marcarComoEntregado(idProducto,idReparte);
           } else {
             this.checked = false;
           }
@@ -61,11 +61,13 @@ function preguntarConfirmacion() {
   });
 }
 
-function marcarComoEntregado(idProducto) {
+function marcarComoEntregado(idProducto, idReparte) {
   const token = localStorage.getItem("access_token");
   const urlMarcarComoEntregado = "http://localhost:8001/api/productos/modificar/" + idProducto;
+
   const headers = {
     "Authorization": "Bearer " + token,
+    "Content-Type": "application/json",
   };
 
   fetch(urlMarcarComoEntregado, {
@@ -74,7 +76,7 @@ function marcarComoEntregado(idProducto) {
   })
     .then(async response => {
       if (response.ok) {
-        location.reload();
+        modificarFechaRealizacion(idReparte);
       } else {
         const errorMessage = await response.text();
         console.error("Error en la solicitud:", errorMessage);
@@ -83,7 +85,45 @@ function marcarComoEntregado(idProducto) {
     .catch(error => {
       console.error("Error:", error);
     });
-};
+}
+
+function modificarFechaRealizacion(idReparte) {
+  const token = localStorage.getItem("access_token");
+  const urlModificarFecha = `http://localhost:8001/api/reparte/${idReparte}`;
+  const fechaRealizacion = obtenerFecha();
+
+  const headers = {
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json",
+  };
+
+  fetch(urlModificarFecha, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({ fechaRealizacion }),
+  })
+    .then(async response => {
+      if (response.ok) {
+        location.reload();
+      } else {
+        const errorMessage = await response.text();
+        console.error("Error en la solicitud para modificar la fecha:", errorMessage);
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+}
+
+function obtenerFecha() {
+  const ahora = new Date();
+  const year = ahora.getFullYear();
+  const month = (ahora.getMonth() + 1).toString().padStart(2, '0'); 
+  const day = ahora.getDate().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
