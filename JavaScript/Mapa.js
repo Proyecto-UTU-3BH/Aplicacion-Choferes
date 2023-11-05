832296e1edc905e2357154ce739aa71c399f8a67
KRaf22 <rafagonzalezknappe@gmail.com>
2023-11-05 16:50:47 -0300

    Fetch a api transito para obtener las paradas que tiene que hacer un chofer y asi mostrar la ruta a seguir en el transporte de lotes. Se usa openstreetmap con libreria leaflet para tener acceso a un mapa

--- JavaScript/Mapa.js
+++ JavaScript/Mapa.js
@@ -1,11 +1,96 @@
-function iniciarMap(){
-    var coord = {lat:-34.89955 ,lng: -56.13243};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
+var map;
+var userLocation = null;
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+
+    userLocation = e.latlng;
 }
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+function calcularRuta(destinos) {
+    if (destinos.length < 2) {
+        return;
+    }
+
+    if (userLocation) {
+        destinos.unshift(userLocation);
+    }
+
+    console.log(destinos);
+
+    L.Routing.control({
+      waypoints: destinos,
+      routeWhileDragging: true
+  }).addTo(map);
+}
+
+function formatearParadas(paradas) {
+
+    const destinos = paradas.map(parada => ({
+        lat: parseFloat(parada.latitud),
+        lon: parseFloat(parada.longitud)
+    }));
+
+    console.log("Destinos Ruta: " + destinos)
+
+    calcularRuta(destinos);
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const userData = JSON.parse(sessionStorage.getItem("userData"));
+  let idUsuario = userData.idUsuario;
+
+  const token = localStorage.getItem("access_token");
+  const urlMostrarRuta = "http://localhost:8001/api/ruta/mostrarRuta/" + idUsuario;
+  const headers = {
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json"
+  };
+
+  fetch(urlMostrarRuta, {
+    method: "GET",
+    headers: headers,
+  })
+    .then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        const errorMessage = await response.text();
+        console.error("Error en la solicitud:", errorMessage);
+        throw new Error("Error en la solicitud");
+      }
+    })
+    .then((data) => {
+     formatearParadas(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+    
+    map = L.map('map').setView([-33.431118, -56.013162], 8);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+    
+    map.locate({ setView: true, maxZoom: 16 });
+    
+    map.on('locationfound', onLocationFound);
+    
+    map.on('locationerror', onLocationError);
+
+});
+
+
